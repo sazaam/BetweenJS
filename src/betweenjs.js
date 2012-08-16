47e33b6927ca6ebf0335472561d58e70f4be22de
sazaam <sazaam@gmail.com>
2012-08-16 15:28:55 +0200

    updated ClassLite v 1.1

--- src/betweenjs.js
+++ src/betweenjs.js
@@ -1,4 +1,3 @@
-
 var __dependancies__ = [
 	{name:'jquery', url:'./jquery-1.7.1.min.js'}
 ] ;
@@ -51,58 +50,51 @@ var __dependancies__ = [
 })() ;
 
 /* 
-ClassLite Version 1.0
+ClassLite Version 1.1
 author saz aka True | contributor Ornorm
 GNU GPL-General Public License
 copyright sazaam[(at)gmail.com] 2012
 */
-'use strict' ;
-var Class = Class || (function(){
-	var ns = {}, ks, regNS = /::|[.]/ , keep = {'constructor':1 , 'factory':1, 'base':1, 'ns':1} ;
-	return ks = function(namespace, properties, extendclass){
-		var xt = extendclass, path = namespace, cl , spy ;
-		var sp = path.split(regNS) ;
-		var $ns = path.replace(regNS, '.') ;
-		var l = sp.length, p = ns, ch ;
+var Class = (function(){
+	var NS = {}, rNS = /::|[.]/ , kp = {'factory':1, 'base':1, 'ns':1} ; 
+	var ks = function Class(namespace, properties, extendclass){
+		var xt = extendclass, pr = properties, xy = namespace, cl ;
+		var sp = xy.split(rNS), ns = xy.replace(rNS, '.') ;
+		var l = sp.length, p = NS, ch ;
 		for(var i = 0 ; i < l ; i++)
 			ch = sp[i], (i < l - 1) && (p = (!!p[ch] ? p[ch] : (p[ch] = {name:ch}))) ;
 		cl = p[ch] ;
-		if(properties !== undefined) cl = undefined ;
-		else return p[ch] ;
-		return cl || (function(){
-			var sub = !! properties ? properties : {} ;
-			var sup = !!xt ? xt : Object ;
+		return (!!!pr && !!!xt) ? cl : (function(){
+			var s = !! pr ? pr : {} ;
+			var ss = !!xt ? xt : Object ;
 			var T = function(){} ;
-			T.prototype = sup.prototype ;
-			if(sub.constructor == Function){
-				sub.prototype = new T ;
-			}else{
-				sub = (function(def, t){
-					var body = def, p, s, o, T = t || function T(){} ;
-					body.__proto__ = def.constructor ;
-					def = body.constructor ;
-					def.prototype = new T ;
-					for(p in body) {
-						if(p == 'statics') {
-							o = body['statics'] ;
-							for(s in o) {
-								if(o.hasOwnProperty(s) && !(s in keep)) (s == 'initialize') ? (def[s] = o[s]).call(def) : def[s] = o[s] ;
-								else if(s == 'toString') def[s] = o[s] ;
-							}
-							delete body['statics'] ;
-						}else if(p == "constructor") continue;
-						else def.prototype[p] = body[p] ;
-					}
-					return def ;
-				})(sub, T) ;
-			}
-			sub.ns = $ns ;
-			sub.base = sup ;
-			sub.factory = sup.prototype ;
-			sub.prototype.constructor = sub ;
-			return (p[ch] = sub) ;
+			T.prototype = ss.prototype ;
+			if(s.constructor == Function) s.prototype = new T ;
+			else s = mk(s, T) ;
+			s.ns = ns ;
+			s.base = ss ;
+			s.factory = ss.prototype ;
+			s.prototype.constructor = s ;
+			return (p[ch] = s) ;
 		})() ;
 	} ;
+	var mk = ks.make = function make(o, t){
+		var b = o, p, s, k, T = t || function T(){} ;
+		b.__proto__ = o.constructor ;
+		o = b.constructor ;
+		o.prototype = new T ;
+		for(p in b) {
+			if(p == 'statics') {
+				for(s in (k = b['statics']))
+					if(k.hasOwnProperty(s) && !(s in kp)) (s == 'initialize') ? (o[s] = k[s]).call(o) : o[s] = k[s] ;
+					else if(s == 'toString') o[s] = k[s] ;
+				delete b['statics'] ;
+			}else if(p == "constructor" || p == 'init') continue ;
+			else o.prototype[p] = b[p] ;
+		}
+		return o ;
+	}
+	return ks ;
 })() ;
 
 if(Class('naja.net::IEvent') === undefined){ // Forcing Writing Naja evt implementation
